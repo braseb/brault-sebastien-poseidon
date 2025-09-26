d9754884a5dcb94cc4f1c450e52f9ea114d2eb92
Brault sebastien <braseb37@gmail.com>
2025-09-27 00:31:09 +0200

    Recreate project poseidon with sources files

---
+++ src/main/java/com/poseidon/app/domain/Trade.java
@@ -0,0 +1,12 @@
+package com.poseidon.app.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.sql.Timestamp;
+
+
+@Entity
+@Table(name = "trade")
+public class Trade {
+    // TODO: Map columns in data table TRADE with corresponding java fields
+}
